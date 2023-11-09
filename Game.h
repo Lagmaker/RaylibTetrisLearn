f49564b304e7e4aa41e96f8aea2fb1c82a947761
Mykytiuk Dmytro <mykytiuk@students.dut.edu.ua>
2023-11-09 19:36:28 +0100

    Add project files.

---
+++ Game.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <string>
+
+
+class Game
+{
+/*
+	Rule of 5 telling us that if we have created on of its members, than you need to implement them all		
+	Here is the list of all 5 Special Member Functions:	
+	0)	Game();									// Default Constructor 
+	1)	~Game();								// Destructor
+	2)	Game(const Game& other);				// Copy Constructor
+	3)	Game& operator= (const Game& other);	// Copy Assignment Operator
+	4)	Game(Game&& other);						// Move Constructor
+	5)	Game& operator= (Game&& other);			// Move Assignment Operator
+
+	Check what is implemented by default and other stuff: https://www.foonathan.net/images/special-member-functions.png
+
+	If 2 and 3 are DELETED, compiler Fallback the behavior of 4 and 5 so them are also DELETED.
+	But if 2 and 3 are DECLARED(Even without realization), compiler implement 4 and 5 in Fallback Style so they will act like Coping instead of Move
+	{
+	 Game(Game&& other){}; // Move Constructor : Falls back to Copy so DELETED
+	 Game& operator= (Game&& other){}; // Move Assignment Operator : Falls back to Copy so DELETED
+	}
+*/
+public:
+	/* Special Member Functions */
+	Game(int width, int height, std::string title);
+	Game(const Game& other) = delete; // Tells the compiler to delete the default realization of Copy Ctor
+	Game& operator= (const Game& other) = delete; // Tells the compiler to delete the default realization of Copy Assignment
+	~Game() noexcept;
+
+	/* Public Methods */
+	bool GameShouldClose() const;
+	void Tick();
+private:
+	/* Private Methods */
+	void Update();
+	void Draw();
+};
+
---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include "Game.h"
+#include "raylib.h"
+
+
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    Game game{ screenWidth, screenHeight, "Tetris Raylib" };
+
+    
+    //Game game2 = game;  // Coping with default ctor means that dtor will clean same memory both times. That causes crash
+    
+    while (!game.GameShouldClose())
+    {
+        game.Tick();
+    }
+
+
+    return 0;
+}
+
+/* Tutorial #1 : Why we need rule of 5
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    Game game{ screenWidth, screenHeight, "Tetris Raylib" };
+
+    {
+        Game game2 = game;  // Coping with default ctor means that dtor will clean same memory both times. That causes crash
+    }
+    
+    return 0;
+}
+
+*/
